7163ed68da3a9c75d842a182f1e9444a9515b003
lordhidalgo <larcilama@cesde.net>
2026-04-07 22:36:48 -0500

    update next.js to latest

--- components/ui/HeroParallaxDemo.tsx
+++ components/ui/HeroParallaxDemo.tsx
@@ -8,9 +8,7 @@ export default function Hero() {
         GAME <br /> CONNECT
       </h1>
       <p className="max-w-xl text-base md:text-xl mt-6 text-white text-left">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
+        Tu portal definitivo para descubrir, conectar y disfrutar de los mejores juegos. Únete a nuestra comunidad y lleva tu experiencia de juego al siguiente nivel.
       </p>
     </div>
   );
