0bda1b1c3df5b7e6cb83b672cbf54fc3af6fb2be
lordhidalgo <larcilama@cesde.net>
2026-04-07 22:20:54 -0500

    cambios en estructura general

--- supabase/client.ts
+++ supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://pcotsuubigvtlmdqfkdr.supabase.co";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_P-VoswuB8-XvpJJKNJP10w_AnfB7zft";
 
 export const supabase = createClient(supabaseUrl!, supabaseKey!);
