627636bcdd135f9425112a10c31effb7b7a7334c
lordhidalgo <larcilama@cesde.net>
2026-04-07 20:24:53 -0500

    chore: forzar ignorar errores de build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Esto permite que el build termine aunque haya advertencias o errores de ESLint
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Esto permite que el build termine aunque haya errores de tipos
     ignoreBuildErrors: true,
   },
 };
